14c4a2c8436772558d82fdfe41df26c3659c7df4
Tanishq TaNiShQ-000 <tanishq.jaiswal000@gmail.com>
2023-08-10 13:25:48 +0530

    changing the config

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import '/dist/bootstrap/css/bootstrap.min.css';
+import Login from './login'
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap'
+import{container} from 'react-bootstrap'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=1b5b553697174e23a83cd38b903f97ff&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+
+export default function login() {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
